5459bc92048e549e51d624b4d9d8ebc6a13bedf5
Clément Le Provost <clement.leprovost@algolia.com>
2016-09-21 15:06:03 +0200

    [test] Add tests for `HighlightRenderer`

---
+++ Tests/HighlightRendererTest.swift
@@ -0,0 +1,95 @@
+//
+//  Copyright (c) 2016 Algolia
+//  http://www.algolia.com/
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import AlgoliaSearch
+@testable import AlgoliaSearchHelper
+import XCTest
+
+
+extension NSRange: Hashable {
+    public var hashValue: Int { return self.location * 65536 ^ self.length }
+}
+
+public func ==(lhs: NSRange, rhs: NSRange) -> Bool {
+    return lhs.location == rhs.location && lhs.length == rhs.length
+}
+
+
+class HighlightRendererTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    private func checkRanges(string: NSAttributedString, ranges: [NSRange: [String: Any]]) {
+        string.enumerateAttributes(in: NSMakeRange(0, string.length), options: []) { (attributes, range, shouldStop) in
+            guard let expectedAttributes = ranges[range] else {
+                XCTFail("Range [\(range.location), \(range.location + range.length)[ not expected")
+                return
+            }
+            // We cannot easily compare the dictionaries because values don't necessarily conform to `Equatable`.
+            // So we just verify that we have the same key set. For our purposes it's enough, because non highlighted
+            // ranges will have empty attributes.
+            XCTAssertEqual(Array(expectedAttributes.keys), Array(attributes.keys))
+        }
+    }
+    
+    func testRender() {
+        let attributes = ["foo": "bar"]
+        let renderer = HighlightRenderer(highlightAttrs: attributes)
+        let result = renderer.render(text: "Woodstock is <em>Snoopy</em>'s friend")
+        checkRanges(string: result, ranges: [
+            NSMakeRange(0, 13): [:],
+            NSMakeRange(13, 6): attributes,
+            NSMakeRange(19, 9): [:]
+        ])
+    }
+    
+    func testCustomMarkers() {
+        let attributes = ["foo": "bar"]
+        let renderer = HighlightRenderer(highlightAttrs: attributes)
+        renderer.preTag = "<mark>"
+        renderer.postTag = "</mark>"
+        let result = renderer.render(text: "Woodstock is <mark>Snoopy</mark>'s friend")
+        checkRanges(string: result, ranges: [
+            NSMakeRange(0, 13): [:],
+            NSMakeRange(13, 6): attributes,
+            NSMakeRange(19, 9): [:]
+        ])
+    }
+
+    func testCaseSensitivity() {
+        let attributes = ["foo": "bar"]
+        let renderer = HighlightRenderer(highlightAttrs: attributes)
+        renderer.caseSensitive = true
+        let result = renderer.render(text: "Woodstock is <EM>Snoopy</EM>'s <em>friend</em>")
+        checkRanges(string: result, ranges: [
+            NSMakeRange(0, 31): [:],
+            NSMakeRange(31, 6): attributes
+        ])
+    }
+}
