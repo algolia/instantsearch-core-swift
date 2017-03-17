62f12ad6bbaebbed9c522c917221111a74d539a3
Clément Le Provost <clement.leprovost@algolia.com>
2017-03-17 10:03:27 +0100

    [test] Add test for `Throttler`

---
+++ Tests/ThrottlerTest.swift
@@ -0,0 +1,35 @@
+//
+//  ThrottlerTest.swift
+//  InstantSearchCore
+//
+//  Created by Clément Le Provost on 17/03/2017.
+//  Copyright © 2017 Algolia. All rights reserved.
+//
+
+import XCTest
+
+class ThrottlerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
