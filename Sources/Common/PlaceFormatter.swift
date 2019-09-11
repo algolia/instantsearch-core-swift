974f9a521cfcc195437d3782a33be12666b617ff
Vladislav Fitc <vladislav.fitc@algolia.com>
2019-09-11 13:37:26 +0200

    WIP

---
+++ Sources/Common/PlaceFormatter.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceFormatter.swift
+//  InstantSearchCore
+//
+//  Created by Vladislav Fitc on 11/09/2019.
+//  Copyright © 2019 Algolia. All rights reserved.
+//
+
+import Foundation
+
+class PlaceFormatter {
+  
+  let place: Place
+  
+  init(place: Place) {
+    self.place = place
+  }
+  
+  func getOutput() -> String {
+    
+    let name = place.localeNames.first
+    let country = place.country
+    let administrative = place.administrative.first.flatMap { $0 == name ? nil : $0  }
+//    let city = place.
+    
+    return ""
+  }
+  
+}
---
+++ Tests/Integration/PlacesIntegrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesIntegrationTests.swift
+//  InstantSearchCore
+//
+//  Created by Vladislav Fitc on 11/09/2019.
+//  Copyright © 2019 Algolia. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import InstantSearchCore
+
+class PlacesIntegrationTests {
+  
+  
+  
+}
