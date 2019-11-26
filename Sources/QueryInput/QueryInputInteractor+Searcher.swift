a270f865ba086c58bbf9fc3cfab17f77cca75544
Vladislav Fitc <vladislav.fitc@algolia.com>
2019-11-26 14:49:35 +0100

    Implement QueryInputInteractor connections

--- Sources/QueryInput/QueryInputInteractor+Searcher.swift
+++ Sources/QueryInput/QueryInputInteractor+Searcher.swift
@@ -13,26 +13,66 @@ public enum SearchTriggeringMode {
   case searchOnSubmit
 }
 
-extension QueryInputInteractor {
-
-  public func connectSearcher<S: Searcher>(_ searcher: S, searchTriggeringMode: SearchTriggeringMode = .searchAsYouType) {
+public extension QueryInputInteractor {
+  
+  struct SearcherConnection<S: Searcher>: Connection {
     
-    query = searcher.query
+    public let interactor: QueryInputInteractor
+    public let searcher: S
+    public let searchTriggeringMode: SearchTriggeringMode
     
-    switch searchTriggeringMode {
-    case .searchAsYouType:
-      onQueryChanged.subscribe(with: searcher) { searcher, query in
-        searcher.query = query
-        searcher.search()
-      }
+    public init(interactor: QueryInputInteractor,
+                searcher: S,
+                searchTriggeringMode: SearchTriggeringMode = .searchAsYouType) {
+      self.interactor = interactor
+      self.searcher = searcher
+      self.searchTriggeringMode = searchTriggeringMode
+    }
+    
+    public func connect() {
       
-    case .searchOnSubmit:
-      onQuerySubmitted.subscribe(with: searcher) { searcher, query in
-        searcher.query = query
-        searcher.search()
+      interactor.query = searcher.query
+      
+      switch searchTriggeringMode {
+      case .searchAsYouType:
+        interactor.onQueryChanged.subscribe(with: searcher) { searcher, query in
+          searcher.query = query
+          searcher.search()
+        }
+        
+      case .searchOnSubmit:
+        interactor.onQuerySubmitted.subscribe(with: searcher) { searcher, query in
+          searcher.query = query
+          searcher.search()
+        }
       }
     }
+    
+    public func disconnect() {
+      
+      interactor.query = nil
+      
+      switch searchTriggeringMode {
+      case .searchAsYouType:
+        interactor.onQueryChanged.cancelSubscription(for: searcher)
+        
+      case .searchOnSubmit:
+        interactor.onQuerySubmitted.cancelSubscription(for: searcher)
+      }
 
+    }
+    
+  }
+  
+}
+
+public extension QueryInputInteractor {
+
+  @discardableResult func connectSearcher<S: Searcher>(_ searcher: S,
+                                                       searchTriggeringMode: SearchTriggeringMode = .searchAsYouType) -> SearcherConnection<S> {
+    let connection = SearcherConnection(interactor: self, searcher: searcher, searchTriggeringMode: searchTriggeringMode)
+    connection.connect()
+    return connection
   }
   
 }
