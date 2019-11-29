8335733cf173db8af1b56959a99fa9e0a0feaf13
Vladislav Fitc <vladislav.fitc@algolia.com>
2019-11-29 11:24:45 +0100

    Implement comparison filter connector

---
+++ Sources/Common/ConnectionHandler.swift
@@ -0,0 +1,9 @@
+//
+//  ConnectionHandler.swift
+//  InstantSearchCore
+//
+//  Created by Vladislav Fitc on 26/11/2019.
+//  Copyright © 2019 Algolia. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Hits/HitsConnector.swift
@@ -0,0 +1,9 @@
+//
+//  HitsConnector.swift
+//  InstantSearchCore
+//
+//  Created by Vladislav Fitc on 29/11/2019.
+//  Copyright © 2019 Algolia. All rights reserved.
+//
+
+import Foundation
