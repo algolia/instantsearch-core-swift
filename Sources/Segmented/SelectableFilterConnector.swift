cc3c0c58478b244297ebca7d689aeb7288141ba3
Vladislav Fitc <vladislav.fitc@algolia.com>
2019-11-29 14:53:24 +0100

    Implement selectable filter connector

---
+++ Sources/Segmented/SelectableFilterConnector.swift
@@ -0,0 +1,53 @@
+//
+//  SelectableFilterConnector.swift
+//  InstantSearchCore
+//
+//  Created by Vladislav Fitc on 29/11/2019.
+//  Copyright © 2019 Algolia. All rights reserved.
+//
+
+import Foundation
+
+public class SelectableFilterConnector<Filter: FilterType>: Connection {
+  
+  public let searcher: SingleIndexSearcher
+  public let filterState: FilterState
+  public let interactor: SelectableSegmentInteractor<Int, Filter>
+  public let attribute: Attribute
+  public let `operator`: RefinementOperator
+  public let groupName: String
+  
+  public let searcherConnection: SelectableFilterInteractorSearcherConnection<Filter>
+  public let filterStateConnection: SelectableFilterInteractorFilterStateConnection<Filter>
+  
+  public init(searcher: SingleIndexSearcher,
+              filterState: FilterState,
+              items: [Int: Filter],
+              attribute: Attribute,
+              `operator`: RefinementOperator,
+              groupName: String? = nil) {
+    self.searcher = searcher
+    self.filterState = filterState
+    self.interactor = .init(items: items)
+    self.attribute = attribute
+    self.operator = `operator`
+    self.groupName = groupName ?? attribute.name
+    self.searcherConnection = self.interactor.connectSearcher(searcher,
+                                                              attribute: attribute)
+    self.filterStateConnection = self.interactor.connectFilterState(filterState,
+                                                                    attribute: attribute,
+                                                                    operator: `operator`,
+                                                                    groupName: groupName)
+  }
+
+  public func connect() {
+    searcherConnection.connect()
+    filterStateConnection.connect()
+  }
+  
+  public func disconnect() {
+    searcherConnection.disconnect()
+    filterStateConnection.disconnect()
+  }
+  
+}
