b4fb42570c738d9be9ce1b1c52abe3e22ef34499
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-01-09 15:34:41 +0100

    Add Insights dependency to Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url:"https://github.com/algolia/algoliasearch-client-swift", .exact("7.0.2"))
+      .package(url:"https://github.com/algolia/algoliasearch-client-swift", .exact("7.0.2")),
+      .package(url:"https://github.com/algolia/instantsearch-ios-insights", .exact("2.3.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
